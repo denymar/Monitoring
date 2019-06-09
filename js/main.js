0772af68ed012bf92f866a3eaaf6ced706bf5bd5
denymar <denymar4yk@gmail.com>
2019-06-09 15:47:54 +0300

    added modals

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
+const logo = document.querySelector(".logo");
+logo.onclick = () => {
+  location.reload(true);
+}
+
 const mainMenu = document.querySelector(".main-menu");
 const mainMenuToggle = document.querySelector(".main-menu-toggle");
 
 mainMenuToggle.onclick = () => {
   mainMenu.classList.toggle("slide-left");
+}
+
+const loginModal = document.querySelector(".modal-login");
+const loginBtn = document.querySelector(`.auth-block button[name="login-btn"]`);
+
+loginBtn.onclick = () => {
+  loginModal.classList.add("modal-show");
+}
+
+loginModal.onclick = (e) => {
+  if (e.target === loginModal) {
+    loginModal.classList.remove("modal-show");
+  }
+}
+
+const signupModal = document.querySelector(".modal-signup");
+const singupBtn = document.querySelector(`.auth-block button[name="signup-btn"]`);
+
+singupBtn.onclick = () => {
+  signupModal.classList.add("modal-show");
+}
+
+signupModal.onclick = (e) => {
+  if (e.target === signupModal) {
+    signupModal.classList.remove("modal-show");
+  }
 }
